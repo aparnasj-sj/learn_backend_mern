4cff7b2da95efec38b2c86f4a35e2e477cb835fe
aparna18cs <aparnasreekumar.18cs028@mbcet.ac.in>
2022-03-06 13:30:28 +0530

    axios sign up

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express'); /// returns a function
 const app = express(); // invoke the function
 
-app.get('/' , (req,res)=>{
-// 200 status code means OK
-res.sendFile('./views/index.html',{root:__dirname});
-})
-app.get('/about' , (req,res)=>{
-    // 200 status code means OK
-    res.send("hello about ");
-    })
+
     
 // Server setup
 app.listen(3000 , ()=>{
 	console.log("server running");
 });
+//Calling the express.json() method for parsing
+app.use(express.json());
+const authRouter = express.Router()
+app.use('/auth',authRouter);
+authRouter.route('/signup')
+.get(getSignUp)
+.post(postSignUp);
+function getSignUp(req,res){
+  res.sendFile('/public/index.html',{root:__dirname}) ;
+}
+function postSignUp(req,res){
+    let obj=req.body;
+    res.json({
+        message:'usersigned up ',
+        data:obj
+    });
+}
+
+
+
+
