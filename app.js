e7d9b91a77d677ce4931d1d3b60ca54f30950a89
aparna18cs <aparnasreekumar.18cs028@mbcet.ac.in>
2022-03-08 04:40:02 +0530

    CRUD mongoose

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'); /// returns a function
 const app = express(); // invoke the function
-
+const mongoose = require('mongoose');
 
     
 // Server setup
@@ -9,22 +9,104 @@ app.listen(3000 , ()=>{
 });
 //Calling the express.json() method for parsing
 app.use(express.json());
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
 const authRouter = express.Router()
+const router = express.Router()
 app.use('/auth',authRouter);
+app.use('/users',router);
 authRouter.route('/signup')
 .get(getSignUp)
 .post(postSignUp);
+router.route('/')
+.get(getUsers)
+.post(postUsers)
+.patch(updateUsers)
+.delete(deleteUsers);
 function getSignUp(req,res){
-  res.sendFile('/public/index.html',{root:__dirname}) ;
+    console.log('here');
+  res.sendFile('/public/index2.html',{root:__dirname}) ;
 }
-function postSignUp(req,res){
-    let obj=req.body;
-    res.json({
-        message:'usersigned up ',
-        data:obj
-    });
+async function postSignUp(req,res){
+    let dataObj=req.body;
+    console.log(dataObj);
+    let user=await userModel.create(dataObj);
+
+    res.json({'meaasge':'user signed up',data:user});
+  
 }
 
+// users route functions
+async function getUsers(req,res){
+    // from DB
+    let allUsers=await userModel.find({});
+    res.json({'message':'users',data:allUsers});
+}
+async function postUsers(req,res){
+    // from DB
+   // let allUsers=await userModel.find();
+   // res.json({'message':'users',data:allUsers});
+}
+async function updateUsers(req,res){
+    let dataToBeUpdated=req.body;
+    let user=await userModel.findOneAndUpdate({email:"abc@gmail.com"}, dataToBeUpdated,{new: true})
+    console.log(dataToBeUpdated,user);
+    res.json({'meassde':'updated user',data:user});
+}
+async function deleteUsers(req,res){
+    let dataToBeDeleted=req.body;
+    let user=await userModel.findOneAndDelete(dataToBeDeleted)
+    console.log(dataToBeDeleted,user);
+    res.json({'meassde':'updated user',data:user});
+}
+const db_link='mongodb+srv://admin:adminadmin321@cluster0.ie8ur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(db_link)// promise based hence can put then
+.then(function(db){// gets a database
+console.log('db connected');
+})
+.catch(function(err){
+    console.log(err);
+})
+//schema 
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    }
+    , password:{
+        type:String,
+        required:true,
+        minLength:5
+    
 
+    },
+    confirmPassword:{
+        type:String,
+        required:true
+        ,minLength:5
 
+    }
+});
 
+// model 
+const userModel = mongoose.model('userModel', userSchema);
+  // name of db , which schema its mapped 
+ /*(async function createUser(){
+      let user={
+          name:'abhi',
+          email:'abc@gmail.com',
+          password:'1234567',
+          confirmPassword:'1234567'
+      }
+      // using functions in model we wanna insert tis to db
+    let data= await userModel.create(user);
+    console.log(data); 
+  })();*/
